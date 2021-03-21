556ea40275ef21154be7ff1f8f73f0d66be07b62
Tristan Miller <tristanmiller89@gmail.com>
2021-03-21 00:00:22 -0500

    finished steps 3 and 4

--- src/components/tabs.js
+++ src/components/tabs.js
@@ -1,3 +1,5 @@
+import axios from 'axios'
+
 const Tabs = (topics) => {
   // TASK 3
   // ---------------------
@@ -13,6 +15,22 @@ const Tabs = (topics) => {
   //   <div class="tab">technology</div>
   // </div>
   //
+  // Instantiating
+  const container = document.createElement('div')
+
+  // Going through topics array and creating elements, adding data, and appending them to the DOM
+  topics.forEach(topic => {
+    const subject = document.createElement('div')
+    subject.classList.add('tab')
+    subject.textContent = topic
+    container.appendChild(subject)
+  })
+
+  // Classes
+  container.classList.add('topics')
+
+  // Return
+  return container
 }
 
 const tabsAppender = (selector) => {
@@ -23,6 +41,16 @@ const tabsAppender = (selector) => {
   // Find the array of topics inside the response, and create the tabs using the Tabs component.
   // Append the tabs to the element in the DOM that matches the selector passed to the function.
   //
+
+  // Append location
+  const appendLocation = document.querySelector(selector)
+
+  // Getting data from endpoint
+  axios.get('https://lambda-times-api.herokuapp.com/topics')
+    .then(res => {
+      // Appending data
+      appendLocation.appendChild(Tabs(res.data.topics))
+    })
 }
 
 export {
